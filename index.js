dc8fd19232ee4ac25f75e44f4a19ac592f1701be
Hugo <hugo.denis22@gmail.com>
2020-11-12 20:24:21 +0100

    add express, body-parser, routes file

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const routes = require('./app/routes')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(routes)
+
+app.listen(8080, () => console.log("server is running.."))
